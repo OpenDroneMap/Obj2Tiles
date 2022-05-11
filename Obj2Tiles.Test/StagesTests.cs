6dd441a717d044e30005c23f039d5f7e340322de
Luca Di Leo <admin@lucadileo.it>
2022-05-11 11:51:04 +0200

    Working on tiling + added test data

---
+++ Obj2Tiles.Test/StagesTests.cs
@@ -0,0 +1,36 @@
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Obj2Tiles.Stages;
+
+namespace Obj2Tiles.Test;
+
+public class StagesTests
+{
+    private const string TestDataPath = "TestData";
+    private const string TestOutputPath = "TestOutput";
+
+    private string GetTestOutputPath(string testName)
+    {
+        var folder = Path.Combine(TestOutputPath, testName);
+        if (Directory.Exists(folder))
+            Directory.Delete(folder, true);
+        Directory.CreateDirectory(folder);
+        return folder;
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        Directory.CreateDirectory(TestOutputPath);
+    }
+
+    [Test]
+    public async Task TilingStage_Tile()
+    {
+        var testPath = GetTestOutputPath(nameof(TilingStage_Tile));
+
+        await StagesFacade.Tile("TestData/Tile1", testPath, 1);
+        
+    }
+}
--- Obj2Tiles/Stages/TilingStage.cs
+++ Obj2Tiles/Stages/TilingStage.cs
@@ -1,4 +1,7 @@
-﻿using Arctron.Obj23dTiles;
+﻿using System.Diagnostics;
+using System.Text;
+using Arctron.Obj23dTiles;
+using Arctron.Obj2Gltf;
 
 namespace Obj2Tiles.Stages;
 
@@ -7,12 +10,35 @@ public static partial class StagesFacade
 
     public static async Task Tile(string sourcePath, string destPath, int lods)
     {
+
+        for (var lod = 0; lod < lods; lod++)
+        {
+            var files = Directory.GetFiles(Path.Combine(sourcePath, "LOD-" + lod), "*.obj");
+
+            foreach (var file in files)
+            {
+                
+                var outputFolder = Path.Combine(destPath, "LOD-" + lod);
+                Directory.CreateDirectory(outputFolder);
+                
+                var outputFile = Path.Combine(outputFolder, Path.ChangeExtension(Path.GetFileName(file), ".b3dm"));
+                //var obj = TilesConverter.WriteB3dm(file, outputFile, null);
+                
+                //Debug.WriteLine(obj.ToString());
+                
+                var outputFile2 = Path.Combine(outputFolder, Path.ChangeExtension(Path.GetFileName(file), ".gltf"));
+                var opts = new GltfOptions { Binary = false, WithBatchTable = false, ObjEncoding = Encoding.UTF8};
+                var converter = new Converter(file, opts);
+                converter.WriteFile(outputFile2);
+                
+            }
+
+        }
         
-        var files = Directory.GetFiles(Path.Combine(sourcePath, "LOD-0"), "*.obj");
 
-        var converter = new TilesConverter(Path.Combine(sourcePath, "LOD-0"), destPath, new GisPosition());
+        //var converter = new TilesConverter(Path.Combine(sourcePath, "LOD-0"), destPath, new GisPosition());
 
-        converter.Run();
+        //converter.Run();
 
 
     }
