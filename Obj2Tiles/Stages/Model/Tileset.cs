687e984464f22ee2b90f31ab73070908f20e6090
Luca Di Leo <admin@lucadileo.it>
2022-05-18 19:20:54 +0200

    Added nullable types

--- Obj2Tiles/Stages/Model/Tileset.cs
+++ Obj2Tiles/Stages/Model/Tileset.cs
@@ -5,51 +5,51 @@ namespace Obj2Tiles.Stages.Model;
 public class Asset
 {
     [JsonProperty("version")]
-    public string Version { get; set; }
+    public string? Version { get; set; }
 }
 
 public class BoundingVolume
 {
     [JsonProperty("box")]
-    public double[] Box { get; set; }
+    public double[]? Box { get; set; }
 }
 
 public class Content
 {
     [JsonProperty("uri")]
-    public string Uri { get; set; }
+    public string? Uri { get; set; }
 }
 
 public class Tileset
 {
     [JsonProperty("asset")]
-    public Asset Asset { get; set; }
+    public Asset? Asset { get; set; }
 
     [JsonProperty("geometricError")]
     public double GeometricError { get; set; }
 
     [JsonProperty("root")]
-    public TileElement Root { get; set; }
+    public TileElement? Root { get; set; }
 }
 
 public class TileElement
 {
     [JsonProperty("transform")]
-    public double[] Transform { get; set; }
+    public double[]? Transform { get; set; }
 
     [JsonProperty("boundingVolume")]
-    public BoundingVolume BoundingVolume { get; set; }
+    public BoundingVolume? BoundingVolume { get; set; }
 
     [JsonProperty("geometricError")]
     public double GeometricError { get; set; }
 
     [JsonProperty("refine")]
-    public string Refine { get; set; }
+    public string? Refine { get; set; }
 
     [JsonProperty("content")]
-    public Content Content { get; set; }
+    public Content? Content { get; set; }
 
     [JsonProperty("children")]
-    public List<TileElement> Children { get; set; }
+    public List<TileElement>? Children { get; set; }
 }
 
