03dbea827ab64ef8849b7f2bcaedd2742da3e8ab
Luca Di Leo <admin@lucadileo.it>
2022-05-25 12:13:48 +0200

    Fixed index length logic

--- Obj2Gltf/WaveFront/Extensions.cs
+++ Obj2Gltf/WaveFront/Extensions.cs
@@ -6,6 +6,6 @@ namespace SilentWave.Obj2Gltf.WaveFront
 {
     static class Extensions
     {
-        public static bool RequiresUint32Indices(this ObjModel objModel) => objModel.Vertices.Count > 65534;
+        public static bool RequiresUint32Indices(this ObjModel objModel) => objModel.Vertices.Count > 65534 || objModel.Uvs.Count > 65534;
     }
 }
