0d7978ee3c96c43c4d6a8f8854605a604bee1b25
Luca Di Leo <admin@lucadileo.it>
2023-04-03 12:59:03 +0200

    Cleanup

--- Obj2Tiles.Test/StagesTests.cs
+++ Obj2Tiles.Test/StagesTests.cs
@@ -91,13 +91,5 @@ public class StagesTests
         Utils.ConvertB3dm("TestData/Tile2/Mesh-XL-YR-XR-YL.obj", Path.Combine(testPath, "out.b3dm"));
 
     }
-    
-    [Test]
-    public void TilingStage_ConvertTest_Problematic()
-    {
-        var testPath = GetTestOutputPath(nameof(TilingStage_ConvertTest));
-        
-        Utils.ConvertB3dm("TestData/Tile3/Mesh-XR-YR.obj", Path.Combine(testPath, "out-p.b3dm"));
 
-    }
 }
